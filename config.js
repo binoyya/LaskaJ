00ec3ee2b9cca4a00dc0d98df416b36677adf423
Binoy Karunarathne <binoykarunarathne@Aeturnums-MacBook-Pro.local>
2019-07-10 13:04:24 +0530

    remove gitignore files

--- config.js
+++ config.js
@@ -3,10 +3,10 @@ module.exports = {
     theme: "lightBlue",
 
     //Email Account
-    host: "smtp.mailtrap.io", //Change to your email smtp
+    host: "smtp.gmail.com", //Change to your email smtp
     port: "465", //Change to your email port
-    user: "user", //Change to your email username
-    pass: "password", //Change to your email password
+    user: "laskajewellery@gmail.com", //Change to your email username
+    pass: "yohan!23", //Change to your email password
 
     //Email Message
     from: '"Contact Me" <noreply@example.com>', // Sender address
